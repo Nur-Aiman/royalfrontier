106fd96713a63ffad5d2e441abb9efde13b3a37a
Nur-Aiman <64392281+Nur-Aiman@users.noreply.github.com>
2023-09-29 23:34:15 +0800

    update registration controller

--- src/main/java/com/restaurant_management_system/model/UserDB.java
+++ src/main/java/com/restaurant_management_system/model/UserDB.java
@@ -8,21 +8,21 @@ import com.restaurant_management_system.beans.user;
 import java.sql.SQLException;
 
 public class UserDB {
-	
-	String s1 = null;
+    
+    String s1 = null;
 
-	public String insertUser(user rb) {
-		myDatabase db = new myDatabase();
-		Connection con = db.getCon();
-		try {
-			Statement stat = con.createStatement();
-			stat.executeUpdate("INSERT INTO \"user\"(name, email, password) VALUES('" + rb.getName() + "','" + rb.getEmail()
-					+ "','" + rb.getPassword() + "')");
-			s1 = "Data Insert Successfully";
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
-		return s1;
-	}
-
+    public String insertUser(user rb) {
+        myDatabase db = new myDatabase();
+        Connection con = db.getCon();
+        try {
+            Statement stat = con.createStatement();
+            stat.executeUpdate("INSERT INTO `user`(name, email, password) VALUES('" + rb.getName() + "','" + rb.getEmail()
+                    + "','" + rb.getPassword() + "')");
+            s1 = "Data Insert Successfully";
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return s1;
+    }
 }
+
